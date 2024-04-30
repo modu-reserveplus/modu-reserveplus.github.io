37f12ea2bbf8997a5f9ca3e70e7918d04d6368c6
olton <olton@users.noreply.github.com>
2024-04-30 11:34:29 +0000

    deploy: modu-main/reserveplus-website@c9b90e2ba06fefc9dd5ae58c523ef4648589cda0

---
+++ en/assets/js/5e95c892.99dde7de.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreserveplus=self.webpackChunkreserveplus||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var u=r(4164),a=r(1003),c=r(7559),l=r(2831),n=r(781),p=r(4848);function t(e){return(0,p.jsx)(a.e3,{className:(0,u.A)(c.G.wrapper.docsPages),children:(0,p.jsx)(n.A,{children:(0,l.v)(e.route.routes)})})}}}]);
